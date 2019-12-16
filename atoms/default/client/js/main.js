84832ee48d1aae25ea1ef4880e921c333b3c72a5
Antonio Voce <antonio.voce@guardian.co.uk>
2019-12-16 15:08:29 +0000

    Handle embeds vs interactives

--- atoms/default/client/js/main.js
+++ atoms/default/client/js/main.js
@@ -1,3 +1,16 @@
 var el = document.createElement('script');
 el.src = '<%= atomPath %>/app.js';
 document.body.appendChild(el);
+
+if (window.resize) {
+  const html = document.querySelector('html')
+  const body = document.querySelector('body')
+
+  html.style.overflow = 'hidden'
+  html.style.margin = '0px'
+  html.style.padding = '0px'
+
+  body.style.overflow = 'hidden'
+  body.style.margin = '0px'
+  body.style.padding = '0px'
+}
