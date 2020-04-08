97d63afb97d6873b3704255cb0be87d0f0fa6226
tinius <nkommenda@hotmail.com>
2020-04-08 11:36:28 +0100

    Revert "all trusts"
    This reverts commit e1d881ce8e6183381eb7e916fefe8f92fb54af79.

---
+++ atoms/default/server/render.js
@@ -0,0 +1,5 @@
+import mainHTML from "./atoms/default/server/templates/main.html!text"
+
+export async function render() {
+    return mainHTML;
+} 
--- atoms/nhs-deaths/server/render.js
+++
@@ -1,5 +0,0 @@
-import mainHTML from "./atoms/nhs-deaths/server/templates/main.html!text"
-
-export async function render() {
-    return mainHTML;
-} 
