e8e0373568c44cd0910ac4d97017e118a48c1168
nickjevershed <nick.evershed@gmail.com>
2021-05-26 15:11:24 +1000

    start of a d3 template of the atom template

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,2 +1,60 @@
-// if you want to import a module from shared/js then you can
-// just do e.g. import Scatter from "shared/js/scatter.js"
+import * as d3 from "d3"
+
+function init(results) {
+
+	const container = d3.select("#graphicContainer")
+	const context = d3.select("#graphicContainer")
+
+	var isMobile;
+	var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+	if (windowWidth < 610) {
+			isMobile = true;
+	}	
+
+	if (windowWidth >= 610){
+			isMobile = false;
+	}
+
+	var width = document.querySelector("#graphicContainer").getBoundingClientRect().width
+	var height = width*0.6				
+	var margin = {top: 0, right: 0, bottom: 0, left:0}
+	
+    context.select("#graphicContainer svg").remove();
+    
+    var chartKey = context.select("#chartKey");
+	chartKey.html("");
+
+	var svg = context.select("#graphicContainer").append("svg")
+				.attr("width", width + margin.left + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
+				.attr("id", "svg")
+				.attr("overflow", "hidden");					
+
+	var features = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+}	// end init
+
+
+
+Promise.all([
+	d3.json(`<%= path %>/aus-hex-grid.json`)
+	])
+	.then((results) =>  {
+		init(results[0])
+		var to=null
+		var lastWidth = document.querySelector("#graphicContainer").getBoundingClientRect()
+		window.addEventListener('resize', function() {
+			var thisWidth = document.querySelector("#graphicContainer").getBoundingClientRect()
+			if (lastWidth != thisWidth) {
+				window.clearTimeout(to);
+				to = window.setTimeout(function() {
+					    init(results[0])
+					}, 100)
+			}
+		
+		})
+
+	});
+
--- atoms/default/client/js/main.js
+++ atoms/default/client/js/main.js
@@ -2,8 +2,6 @@ var el = document.createElement('script');
 el.src = '<%= atomPath %>/app.js';
 document.body.appendChild(el);
 
-
-
 setTimeout(() => {
   if (window.resize) {  
     const html = document.querySelector('html')
@@ -20,3 +18,50 @@ setTimeout(() => {
   window.resize()
   }
 },100)
+
+// Amp resize fix
+
+const doResize = () => {
+
+  if (window.resize) {
+      window.resize()
+  }
+
+  window.parent.postMessage({
+      sentinel: 'amp',
+      type: 'embed-size',
+      height: document.body.scrollHeight
+    }, '*')
+}
+
+try {
+  doResize()
+} catch(err) { console.log("Resize error", err) }
+
+
+// Resizes the iframe if embedded in an article
+
+function onElementHeightChange(elm, callback) {
+    var lastHeight = elm.clientHeight, newHeight;
+    (function run(){
+        newHeight = elm.clientHeight;
+        if( lastHeight != newHeight )
+            callback();
+        lastHeight = newHeight;
+
+        if( elm.onElementHeightChangeTimer )
+            clearTimeout(elm.onElementHeightChangeTimer);
+
+        elm.onElementHeightChangeTimer = setTimeout(run, 250);
+    })();
+}
+
+if (window.frameElement) {
+
+    onElementHeightChange(document.body, function() {
+        window.frameElement.height = document.body.offsetHeight + 150
+    });
+
+}
+
+
